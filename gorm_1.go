22c26a4ed3c65a8b9a6a6f1218ef9df95bbc6cc2
asen477 <asen477@gmail.com>
2020-01-14 16:53:41 +0800

    go学习笔记

---
+++ gorm_1.go
@@ -0,0 +1,46 @@
+/**
+ * User: Trunks(Gao)
+ * Date: 2020/01/14
+ */
+package main
+
+import (
+	"fmt"
+	"time"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+//商品
+type Food struct {
+	Id         int
+	Title      string
+	Price      float32
+	Stock      int
+	Type       int
+	//mysql datetime, date类型字段，可以和golang time.Time类型绑定， 详细说明请参考：gorm连接数据库章节。
+	CreateTime time.Time
+}
+
+//为Food绑定表名
+func (v Food) TableName() string {
+	return "foods"
+}
+
+func main(){
+
+	db, err := gorm.Open("mysql", "root:root@/golang?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err.Error()) //错误处理
+	}
+
+	//定义接收查询结果的结构体变量
+	food := Food{}
+
+	//等价于：SELECT * FROM `foods`   LIMIT 1
+	//db.Take(&food)
+	db.First(&food,1)
+
+	fmt.Println(food)
+}
+
---
+++ number.go
@@ -0,0 +1,29 @@
+/**
+ * User: Trunks(Gao)
+ * Date: 2019/11/09
+ */
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func main(){
+	fmt.Print(IfPrime(77))
+}
+
+//函数判断是否是质数
+func IfPrime(value int) (bool, error) {
+	if value < 2 {
+		return false, errors.New("小于2的整数无法判断")
+	}
+	end := int(math.Sqrt(float64(value)))
+	for i := 2; i <= end; i++ {
+		if value%i == 0 {
+			return false, errors.New("不能被自己整除")
+		}
+	}
+	return true, errors.New("是质数")
+}
