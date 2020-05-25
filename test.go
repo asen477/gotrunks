28675803ebaef02de1593c7ab71c83677c046660
asen477 <asen477@gmail.com>
2020-05-25 11:47:21 +0800

    新增测试文件，开启web

---
+++ test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var workDay = [7]string{"星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"}
+
+	fmt.Printf("%v\n", workDay)
+
+	//for {
+	//	fmt.Printf("Works%\n",workDay)
+	//}
+}
---
+++ web.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func myWeb(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "这是一个开始")
+}
+
+func main() {
+	http.HandleFunc("/", myWeb)
+
+	//fmt.Println("服务器即将开启，访问地址 http://140.143.205.57:9999")
+	fmt.Println("服务器即将开启，访问地址 http://127.0.0.1:80")
+
+	err := http.ListenAndServe(":9999", nil)
+	if err != nil {
+		fmt.Println("服务器开启错误: ", err)
+	}
+}
